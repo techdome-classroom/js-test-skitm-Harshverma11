0d8f5afcbdc42df62a302fd3c8e777cdf11d2808
Harshverma11 <128955303+Harshverma11@users.noreply.github.com>
2024-01-12 14:13:41 +0530

    Update program1.js
    program1 solution

--- program1.js
+++ program1.js
@@ -2,7 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+    def isValid(s):
+    stack = []
+    brackets = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in brackets.values():
+            stack.append(char)
+        elif char in brackets.keys():
+            if not stack or stack.pop() != brackets[char]:
+                return False
+        else:
+            return False
+
+    return not stack
+
+
     
 };
 
