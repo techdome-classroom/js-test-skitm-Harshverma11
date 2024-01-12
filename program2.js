02bb60f6c56d20b805b268a8fcfd9b69b3675d36
Harshverma11 <128955303+Harshverma11@users.noreply.github.com>
2024-01-12 14:16:48 +0530

    Update program2.js
    program2's solution

--- program2.js
+++ program2.js
@@ -3,7 +3,19 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    def romanToInt(s):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+
+    for i in range(len(s)):
+        # If the current numeral is smaller than the next one, subtract its value
+        if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i + 1]]:
+            result -= roman_dict[s[i]]
+        else:
+            result += roman_dict[s[i]]
+
+    return result
+
 };
 
 
